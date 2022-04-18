a701088265f52fed5aa4794b73022cc975cb7183
Dev_Shiv <im.shiv.579@gmail.com>
2022-04-18 17:10:17 +0530

    styling HeroSection background

--- src/components/Herosection/Herosection.jsx
+++ src/components/Herosection/Herosection.jsx
@@ -5,14 +5,14 @@ function Herosection() {
     <div className="heroSection-container flex items-center justify-center p-8 h-screen w-screen relative">
       <div className="bg-video absolute top-0 right-0 left-0 bottom-0 -z-10">
         <video
-          className="w-screen h-screen object-cover -z-10"
+          className="w-screen h-screen object-cover -z-10 saturate-0	"
           autoPlay="true"
           loop="true"
           muted="true"
           typeof="video/mp4"
           poster="./images/bg-videoPoster.png"
         >
-          <source src="./videos/bg-video.mp4" type="video/mp4" />
+          <source src="./videos/bg-video2.mp4" type="video/mp4" />
         </video>
       </div>
       <div className="text-wrapper h-fit max-w-fit">
