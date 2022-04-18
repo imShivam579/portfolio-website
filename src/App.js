5fe1ce5e5748aa16cd8bbbea9ef1430a3df49bad
Dev_Shiv <im.shiv.579@gmail.com>
2022-04-18 17:31:40 +0530

    Adding Projects wrapper component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Herosection from "./components/Herosection/Herosection";
+import Projects from "./components/Projects/Projects";
 function App() {
   return (
     <div className="App relative">
       <Navbar />
       <Herosection />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div className="container mx-auto">
+      <div className="title">Selected Projects</div>
+      {/* Projects wrapper  */}
+      <div className="wrapper">
+        {/* card  */}
+        {/* card  */}
+        {/* card  */}
+        {/* card  */}
+      </div>
+      {/* link  */}
+      <span className="block">
+        <a href="#">View All</a>
+      </span>
+    </div>
+  );
+}
+
+export default Projects;
