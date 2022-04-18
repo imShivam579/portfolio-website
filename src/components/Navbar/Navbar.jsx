8599f6f34a533878b16f907bb83b94e6492d43d9
Dev_Shiv <im.shiv.579@gmail.com>
2022-04-18 12:32:17 +0530

    completed navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 function Navbar() {
   const [isMenuOpen, setMenuOpen] = useState(false);
   function toggleMenu() {
-    console.log("click");
     setMenuOpen(!isMenuOpen);
   }
   return (
@@ -14,19 +13,19 @@ function Navbar() {
       {/* navbar right - nav menu  */}
       <div className="nav-right hidden md:block">
         <ul className="flex justify-between">
-          <li className="px-4 fs-link-rg">
+          <li className="px-4">
             <a href="#">Work</a>
           </li>
-          <li className="px-4 fs-link-rg">
+          <li className="px-4">
             <a href="#">About me</a>
           </li>
-          <li className="px-4 fs-link-rg">
+          <li className="px-4">
             <a href="#">Contact</a>
           </li>
         </ul>
       </div>
       {/* navbar right - burger menu  */}
-      <button onClick={toggleMenu} className="cursor-pointer md:hidden">
+      <button onClick={toggleMenu} className="md:hidden">
         <svg
           width="32"
           height="32"
@@ -59,19 +58,56 @@ function Navbar() {
       </button>
 
       <div
-        className={`burger-menu absolute top-0 bottom-0 right-0 w-screen h-screen md:hidden  ${
-          !isMenuOpen ? "hidden" : null
+        className={`burger-menu absolute top-0 bottom-0 right-0 w-screen h-screen md:hidden bg-black ${
+          !isMenuOpen ? "hidden" : ""
         }`}
       >
+        <button onClick={toggleMenu} className="absolute top-8 right-8">
+          <svg
+            width="32"
+            height="32"
+            viewBox="0 0 25 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M7 17L21 17"
+              stroke="#8E95A9"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M4 12H18"
+              stroke="#8E95A9"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M7 7H21"
+              stroke="#8E95A9"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </button>
         <ul className="h-full w-full flex flex-col justify-center items-center">
-          <li className="px-4 h2">
-            <a href="#">Work</a>
+          <li className="px-4">
+            <a href="#" className="h2">
+              Work
+            </a>
           </li>
-          <li className="px-4 h2">
-            <a href="#">About me</a>
+          <li className="px-4">
+            <a href="#" className="h2">
+              About me
+            </a>
           </li>
-          <li className="px-4 h2">
-            <a href="#">Contact</a>
+          <li className="px-4">
+            <a href="#" className="h2">
+              Contact
+            </a>
           </li>
         </ul>
       </div>
