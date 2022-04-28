bf3b1df37d35f5ad0b92cd3eeec598a631b3bd4f
Dev_Shiv <im.shiv.579@gmail.com>
2022-04-28 21:45:29 +0530

    Refractoring Herosection 0.2

--- src/components/Herosection/Herosection.jsx
+++ src/components/Herosection/Herosection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Herosection() {
   return (
-    <div className="heroSection h-max  w-screen relative pt-60 pb-60 bg-black-100">
+    <div className="heroSection h-max  w-screen relative bg-black-100">
       <div className="bg-video absolute top-0 right-0 left-0 bottom-0 -z-10">
         <video
           className="w-screen h-screen object-cover -z-10 saturate-0	"
@@ -15,12 +15,12 @@ function Herosection() {
           <source src="./videos/bg-video2.mp4" type="video/mp4" />
         </video>
       </div>
-      <div className="wrapper">
-        <div className="hero-content relative left-0 text-left">
+      <div className="wrapper relative">
+        <div className="hero-content relative left-0 text-left pointer-events-none">
           <h4 className="text-gray-200 h4">Shivam sharma</h4>
           <h1 className="text-gray-50 mt-8 relative left-0">
-            <span className="block relative left-0">Web Developer</span>
-            <span className="block relative left-1/3">Designer</span>
+            <span className="line block">Web Developer</span>
+            <span className="line block">Designer</span>
           </h1>
         </div>
       </div>
